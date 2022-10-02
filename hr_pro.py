d7878ebc50205d6e800196c15a97ad72cf93747d
athouba99 <athooba99@gmail.com>
2022-10-02 18:45:37 +0300

    hr_pro

--- hr_pro.py
+++ hr_pro.py
@@ -1,8 +1,29 @@
-class Employee:
+class Employee:  #class 1
    #Employee class here
+    def __init__(self,name,age, salary, employment_years):
+        self.name = name
+        self.age = age
+        self.salary = salary
+        self.employment_years = employment_years
 
-class Manager(Employee):
+    def get_annual_salary(self,salary):
+        return salary*12 
+
+      
+class Manager(Employee): #class 2 
     #Manager class here
+    def __init__(self, name, age, salary, employment_years, bonus_percentage):
+        super().__init__(name,age, salary, employment_years)
+        self.name = name 
+        self.age = age
+        self.salary = salary
+        self.employment_years = employment_years 
+        self.bonus_percentage = bonus_percentage 
+
+    def get_bonus(self, bonus_percentage):
+        
+
+        
         
 def main():
 	# main code here
