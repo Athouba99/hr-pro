b537f582dc926a30bc38d69e8db9256e357480f9
athouba99 <athooba99@gmail.com>
2022-10-02 20:02:39 +0300

    hr_pro

--- hr_pro.py
+++ hr_pro.py
@@ -22,14 +22,20 @@ class Manager(Employee): #class 2
         self.employment_years = employment_years 
         self.bonus_percentage = bonus_percentage 
 
-    def get_bonus(self, bonus_percentage):
-        return bonus_percentage*12
+    def get_bonus(self, salary):
+        return salary*12
 
     def __str__(self):
       return f"Name:{self.name}, Age:{self.age}, Salary:{self.salary}, Working Years:{self.employment_years}, Bonus:{self.bonus_percentage}"  
   
 def main():
 	# main code here
+    print ("Welcome to HR Pro \nOptions:")
+    lista = ["1. Show Employees","2. Show Managers","3. Add An Employee","4. Add A Manager", "5.Exit"]
+    print (f"{lista[0]} \n{lista[1]} \n{lista[2]} \n{lista[3]} \n{lista[4]}")
+
+    input = int(lista())
+    
     list1 = [] # empty lilst to be filled 
     list1.append(Employee ("laila",24 , 9999, 4))
     list1.append(Employee("Moh", 27, 999, 2))
@@ -43,5 +49,10 @@ def main():
     for obj in list2:
         print(obj.name, obj.age, obj.salary, obj.employment_years, obj.bonus_percentage)
 
+    # lista = ["1. Show Employees","2. Show Managers","3. Add An Employee","4. Add A Manager", "5.Exit"]
+    # for index in enumerate(lista):
+    #     print("What would you like to do?")
+
+   
 if __name__ == '__main__':
 	main()
